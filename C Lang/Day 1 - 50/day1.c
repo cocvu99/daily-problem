587975a2394c1e05758f6b98dad76dd5ecb2a910
Vu Nguyen <cocvu99@gmail.com>
2022-01-19 18:01:41 +0700

    add more language - Problem not done

---
+++ C Lang/Day 1 - 50/day1.c
@@ -0,0 +1,9 @@
+// 22.01.19
+// Khoi tao file
+#include <stdio.h>
+
+int main(){
+    printf("Hello Problem 1 With C lang");
+
+    return 0;
+}
---
+++ C++/Day 51 - 100/day51.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file day51.cpp
+ * @author cocvu
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-19
+ * 
+ * @copyright Copyright (c) 2022
+ * Khoi tao file - NOT DONE
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello Problem 51" << endl;
+}
