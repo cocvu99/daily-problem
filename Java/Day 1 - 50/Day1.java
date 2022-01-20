bc4abee24564a1b197f223d2f32ee59f3d4dc8c5
Vu Nguyen <cocvu99@gmail.com>
2022-01-20 21:13:50 +0700

    day1 java 2vers, day2 python ver1

--- Java/Day 1 - 50/Day1.java
+++ Java/Day 1 - 50/Day1.java
@@ -1,6 +1,65 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
 public class Day1 {
     
+    // one pass ver 1
+    public static boolean containsPairWithSum(int[] a, int x) {
+        Arrays.sort(a);
+        for (int i = 0, j = a.length - 1; i < j;) {
+            int sum = a[i] + a[j];
+            if (sum < x)
+                i++;
+            else if (sum > x)
+                j--;
+            else
+                return true;
+        }
+        return false;
+    }
+
+    2 loop
+    public static boolean FindPairs(int arr[], int k) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] + arr[j] == k) {
+                    return true;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello java Problem 1");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Do dai mang?: ");
+        int number = input.nextInt();
+
+        System.out.print("Number k: ");
+        int k = input.nextInt();
+
+        int[] myArr = new int[number];
+
+        for (int i = 0; i < number; i++) {
+            myArr[i] = input.nextInt();
+        }
+
+        // one pass ver 1
+        if (containsPairWithSum(myArr, k)) {
+            System.out.println("Mang OK");
+        } else {
+            System.out.println("Mang KHONG TMDK");
+        }
+
+        // if (!FindPairs(myArr, k)) {
+        //     System.out.println("Mang? Thoa man DK");
+        // } else {
+        //     System.out.println("Mang? KHONG TMDK");
+        // }
+
+        
     }
 }
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Status:
 
 | Problems/Language     | C Lang   | C++      | Golang | Java | Python|
 | ----------------------|:--------:|:--------:|:--------:|:--------:|:--------:|
-| 1                     | NOT DONE | NOT DONE |NOT DONE |NOT DONE | DONE |
+| 1                     | NOT DONE | NOT DONE |NOT DONE |DONE 2 ver | :heavy_check_mark: :heavy_check_mark: |
 | 2                     | NOT DONE | NOT DONE |NOT DONE |NOT DONE |DONE Ver 1 |
 | 3                     | NOT DONE | NOT DONE |NOT DONE |NOT DONE |NOT DONE |
 | 4                     | NOT DONE | NOT DONE |NOT DONE |NOT DONE |NOT DONE |
